d296364318d2ad2675d8a6bfe21b79861901c9c6
RISHABH SHAH <rnshah1997@gmail.com>
2025-05-19 22:51:45 -0400

    routeUpdate

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./Pages/Home";
+import About from "./Pages/About";
+
 function App() {
-  return <div></div>;
+  const router = createBrowserRouter([
+    { path: "/", element: <Home /> },
+    { path: "/About", element: <About /> },
+  ]);
+  return <RouterProvider router={router} />;
 }
 
 export default App;
